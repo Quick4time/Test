3635ad967bb95820223e30f98736a99781c780a6
Kravchenko Artem <lapanteross@rambler.ru>
2017-06-03 19:27:34 +0900

    Interfaces Exam

---
+++ Assets/Interfaces/Iinterfaces.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Теория интерфейса
+public class Iinterfaces : MonoBehaviour
+{
+    // Свойства int Age {get; set;}(автосвойства) Нужны для добавления проверки входных данных, либо ограничить возможности для чтения и записи данных.
+    // Конвенции програмирования (свод правил программирования)
+    // public int MaxSpeed(С большой буквы PascalCase); private int maxSpeed;(локальные поля классов или аргументы с маленькой буквы camelCase) public class Car; public void StatEngine(){ } Hungarian Notation private int _maxSpeed; Snake_case imena_s_podcherkivaniem(ne_uzatb:))
+    // Рефакторинг - Процесс редактирования кода нацеленный нп улучшение читаемости кода, не изменяя его функциональности
+    // Ctr+K,D Выравниваем скобки
+    // Интерфейс - это именованный набор абстрактных членов(дебильное определение)
+    // Интерфейс - это аспект языка, который служит для того, что бы формировать интерфейс взаимодействия класса(Интерфейс взаимодействия - это совокупность всех публичных членов класса).
+    // Интерфейс это не наследуют, а реализуют
+    // При реализации интерфейса, класс должен иметь все публичные члены интерфейса
+    // Можно добавить свои свойства и методы к классу
+    // Приведение к интерфейсу (upcast) Iinterface interface = new SomeMethod(); interface.Host = "Host"; и т.д Используються все методы и свойства и в классе и интерфейсе.
+    // Класс может реализововать несколько интерфейсов.
+}
+
+interface Iinterface
+{
+    void DoSome();
+}
+interface Iinterface1
+{
+    void DoSome();
+}
+
+class Concrete : Iinterface
+{
+    public void DoSome()
+    {
+        Method(); // Но можем вызвать через любую функцию определенную в интерфейсе (например через DoSome()) Пример Инкапсуляции
+    }
+
+    public void Method() // Метод Method() неопределен в интерфейсе, при приведении к интерфейсу вызвать не сможем.
+    {
+        // Call method
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Iinterface myInterface = new Concrete(); // вот это upcase
+        myInterface.DoSome();
+    }
+}
+
+// Что происходит, если два интерфейса обязывают реализовать одну и ту же функцию'
+// 1-ый способ это когда функция будет с одной и той же реализацией
+
+class Concrete1 : Iinterface, Iinterface1
+{
+    public void DoSome() // 
+    {
+        // Одна и та же реализация
+    }
+}
+// 2-ой путь мы реалзовываем функцию для каждого интерфейса отдельно
+class Concrete2 : Iinterface, Iinterface1
+{
+    void Iinterface1.DoSome()
+    {
+        // реализуем один интерфейс по своему.
+    }
+
+    void Iinterface.DoSome()
+    {
+        // реализуем другой интерфейс отличный от первого.
+    }
+}
+
+// Пример использования интерфейса 
+interface IWriter
+{
+    void Write(string text);
+}
+// Этот интерфейс реализуют сразу несколько классов
+
+// Прямо в ходе программы мы можем пихать это свойства любой класс который реализует данный интерфейс, тем самым мы меняем логику экземпляра который содержиться в данном свойстве
+// В этом примере мы добиваемся взаимозаменяемости классов
+
+class ExampleInterface
+{
+    TextWorker textWorker = new TextWorker(); // upcase
+    
+    void Start()
+    {
+        textWorker.Writer = new StandartWriter();
+        textWorker.WriteText("text"); // результат : text some
+        textWorker.Writer = new BracesWriter();
+        textWorker.WriteText("text"); // результат : {text some}
+        textWorker.Writer = new SquareWriter();
+        textWorker.WriteText("text"); // результат : [text some]
+    }
+}
+
+class TextWorker
+{
+    public IWriter Writer { get; set; }
+
+    public void WriteText (string text)
+    {
+        text += "some";
+        Writer.Write(text);
+    }
+}
+
+class StandartWriter : IWriter
+{
+    public void Write (string text)
+    {
+        Debug.Log(text);
+    }
+}
+
+class BracesWriter : IWriter
+{
+    public void Write(string text)
+    {
+        Debug.Log("{" + text + "}");
+    }
+}
+
+class SquareWriter : IWriter
+{
+    public void Write(string text)
+    {
+        Debug.Log("[" + text + "]");
+    }
+}
---
+++ Assets/Interfaces/InterfaceExample.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+// Пример использования интерфейса
+public class InterfaceExample
+{
+   // Интерфейсы позволяеют добиться взаимозаменяемости частей класса, дабы не писать новвй класс раз за разом.
+}
+
+namespace InterfacePart2
+{
+    class Rocket
+    {
+        public RocketHeader Header { get; set; } // головная часть
+        public IEngine Engine { get; set; }      // двигатель
+
+        public int Weight
+        {
+            get
+            {
+                return Header.GetWeight() + Engine.Weight; // масса головной части + масса движка + масса космонавтов
+            }
+        }
+    }
+
+    class RocketHeader
+    {
+        public int Cosmonauts { get; private set; } // количество космонавтов
+        public int MassShell { get; private set; }  // масса корпуса
+
+        public RocketHeader()
+        {
+            Cosmonauts = 3;
+            MassShell = 5000;
+        }
+
+        public int GetWeight() // возвращает общую массу модуля с учетом веса космонавтов
+        {
+            return (Cosmonauts * 80) + MassShell;
+        }
+
+        public void SendMessage (string message)
+        {
+            Console.WriteLine("Сообщение: ");
+            Console.WriteLine(message);
+            Console.WriteLine("отправлено");
+        }
+    }
+
+    interface IEngine
+    {
+        void Start(); // метод для запуска
+        void Stop();  // метод для остановки
+        int Weight { get; } // масса двигателя
+        int Power { get; }  // мощность двигателя
+    }
+    // Нужен для того что бы класс имел конкретные члены и свойства интерфейса.
+
+    class HatersEngine : IEngine // двигатель летающий на тяге пердаков хуйтеров
+    {
+        public int Weight { get; set; }     // масса двигателя
+        public int Power { get; set; }      // мощность двигателя
+        public string GetCop { get; set; }  // получаем копирайты
+
+        public HatersEngine()
+        {
+            Weight = 322;
+            Power = 228;
+            GetCop = "ExtremeCodeTV EngineCreator";
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Пуканы раскаляются, тяга на максимум");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Вброс годного контента. Пуканы остывают");
+        }
+    }
+
+
+    class CryEngine : IEngine // Двигатель летающий на слезах фанатов сборной России по футболу
+    {
+        public int Weight { get; set; }
+        public int Power { get; set; }
+
+        public CryEngine()
+        {
+            Weight = 800;
+            Power = 2000;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("То что этот двигатель не оправдывает ваши ожидания - ваши проблемы");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Прохождение в плей-оф");
+        }
+
+        public void BeginAdvertising()
+        {
+            Console.WriteLine("Кушайте Лейс - пи*дец как вкусно");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rocket rocket = new Rocket();
+
+            rocket.Header = new RocketHeader(); 
+            rocket.Engine = new HatersEngine(); // upcustom приводим наследника к родителю
+
+            Console.WriteLine("Хейтерская тяга: " + SpecialNasaMethod(rocket.Engine.Power, rocket.Weight));
+
+            rocket.Engine = new CryEngine();
+            Console.WriteLine("На слезах хейтеров: " + SpecialNasaMethod(rocket.Engine.Power, rocket.Weight));
+
+            //rocket.Engine = new PoliticsTalksEngine();
+            //Console.WriteLine("На пи*деже политиков: " + SpecialNasaMethod(rocket.Engine.Power, rocket.Weight));
+
+            //rocket.Engine = new ShitEngine();
+            //Console.WriteLine("На песнях российской эстрады: " + SpecialNasaMethod(rocket.Engine.Power, rocket.Weight));
+
+            //rocket.Engine = new YriyLozaEngine();
+            //Console.WriteLine("На пафосе Юрия Лозы: " + SpecialNasaMethod(rocket.Engine.Power, rocket.Weight));
+
+            //Console.ReadKey();
+
+        }
+
+
+        public static int SpecialNasaMethod(int power, int mass) // метод для вычисления максимальной скорости
+        {
+            return (mass / power * 10) + 82;
+        }
+    }
+}
+
