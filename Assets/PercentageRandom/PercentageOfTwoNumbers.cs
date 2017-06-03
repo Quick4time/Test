7971254ffff125fae2646e0c01150fc94acc60c3
Kravchenko Artem <lapanteross@rambler.ru>
2017-06-03 11:38:59 +0900

    Do some Percentage.cs

---
+++ Assets/PercentageRandom/PercentageOfTwoNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+// выбираем случайное значение в соотношении с долей вероятности
+public class ProportuionValue<T>
+{
+    public float Proportion { get; set;}
+    public T Value { get; set; }
+}
+
+public static class ProportionValue
+{
+    public static ProportuionValue<T> Create<T> (float proportion, T value)
+    {
+        return new ProportuionValue<T> { Proportion = proportion, Value = value };
+    }
+
+    static System.Random random = new System.Random();
+    public static T ChoseByRandom<T>(this IEnumerable<ProportuionValue<T>> collection)
+    {
+        var rnd = random.NextDouble();
+        foreach (var num in collection)
+        {
+            if (rnd < num.Proportion)
+                return num.Value;
+            rnd -= num.Proportion;
+        }
+        throw new InvalidOperationException("The proportions in the collection do not add up to 1."); // Соотношения в коллекции является больше одного или меньше одного (то есть мы привысили процентаж в 100% или <100%).
+    }
+}
+
+// Пример
+public class PercentageOfTwoNumbers : MonoBehaviour
+{
+    private void Update()
+    {
+        var random = new[]
+            { ProportionValue.Create(0.3f, "a"),
+              ProportionValue.Create(0.3f, "b"),
+              ProportionValue.Create(0.4f, "c")
+            };
+        if (Input.anyKeyDown)
+        { 
+            Debug.Log(random.ChoseByRandom().ToString());
+        }
+    }
+}
---
+++ Assets/PercentageRandom/PercentageScr.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PercentageScr : MonoBehaviour
+{
+    // Код на шанс выпадения двух рандомных чисел с какой либо вероятностью.
+    struct RandomSelection
+    {
+        private int minValue;
+        private int maxValue;
+        public float probability;
+
+        public RandomSelection (int minValue, int maxValue, float probability)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.probability = probability;
+        }
+
+        public int GetValue() { return Random.Range(minValue, maxValue + 1); }
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            int random = GetRandomValue(new RandomSelection(4, 15, 0.8f));
+            Debug.Log(random.ToString());
+        }
+    }
+
+    int GetRandomValue(params RandomSelection[] selections)
+    {
+        float rand = Random.value;
+        float currentProb = 0;
+
+        foreach (var selection in selections)
+        {
+            currentProb += selection.probability;
+            if (rand <= currentProb)
+                return selection.GetValue();
+        }
+        //will happen if the input's probabilities sums to less than 1
+        //throw error here if that's appropriate
+        return 0;
+    }
+}
